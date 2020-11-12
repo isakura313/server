c0f5e20c6627ca403ee51bc4fec6575eae0380c7
Paul <isakura313@gmail.com>
2020-11-12 14:46:40 +0300

    asds

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "Im1291821",
-    DB: "TODO"
+    HOST: "us-cdbr-east-02.cleardb.com",
+    USER: "b3ba8534bf15a0",
+    PASSWORD: "249ea70c",
+    DB: "heroku_6c4c353c7979441"
 }
--- data.sql
+++ data.sql
@@ -4,3 +4,4 @@ CREATE TABLE IF NOT EXISTS 'todos' (
   content varchar(255),
   create_date DATE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+mysql://b3ba8534bf15a0:249ea70c@us-cdbr-east-02.cleardb.com/heroku_6c4c353c7979441?reconnect=true
